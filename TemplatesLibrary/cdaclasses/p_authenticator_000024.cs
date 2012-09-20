5ea2ff3cc6b15b0d834ceecd32b43ed94c3f403e
Richard Kavanagh <git@cdaapi.co.uk>
2012-09-20 07:44:09 +0100

    Add authenticator constraints

---
+++ TemplatesLibrary/cdaclasses/p_authenticator_000024.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+using MARC.Everest.DataTypes;
+
+using nhs.itk.hl7v3.templates;
+using nhs.itk.hl7v3.rim;
+using nhs.itk.hl7v3.datatypes;
+using nhs.itk.hl7v3.xml;
+
+namespace nhs.itk.hl7v3.cda.classes
+{
+    public class p_authenticator_000024 : ParticipationClass
+    {
+        public NPFIT_000024_Role Role { get; set; }
+
+        public p_authenticator_000024()
+            : base()
+        {
+            base.typeCode = "AUTHEN";
+        }
+
+        public p_authenticator_000024(TS authenticationTime)
+            : base()
+        {
+            base.typeCode = "AUTHEN";
+            base.time = new IVL<TS>(authenticationTime);
+        }
+
+        public TS AuthenticationTime
+        {
+            set
+            {
+                if (base.time == null)
+                {
+                    base.time = new IVL<TS>();
+                }
+
+                base.time.Value = value;
+            }
+            get
+            {
+                return base.time.Value;
+            }
+        }
+
+        #region XML
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteAttributeString("typeCode", base.typeCode);
+
+            its.TemplateLookAhead(Role.getTemplateID() + "#" + Role.getTemplateText(), writer);
+
+            FormatterHelper.SerialiseDataType(time, writer, "time");
+
+            //
+            // The follow attrbute is fixed, so this is hardcoded as below.
+            //
+            writer.WriteStartElement("signatureCode");
+            writer.WriteAttributeString("nullFlavor", "NA");
+            writer.WriteEndElement();
+
+            writer.WriteStartElement("assignedEntity");
+            Role.WriteXml(writer);
+            writer.WriteEndElement();
+        }
+        #endregion
+    }
+}
---
+++ TemplatesLibrary/constraints/NPFIT-000024-Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace nhs.itk.hl7v3.templates
+{
+    /// <summary>
+    /// Constraint for ClinicalDocument.authenticator
+    /// </summary>
+    public interface NPFIT_000024_Role : ITemplateConstraint
+    { }
+}
